283745fe92bfdd4ae4d5eeeb0231cd168d8d1a2b
zahid-bracu <zahid.mym@gmail.com>
2021-11-25 12:13:09 +0600

    ok

---
+++ python-programming/basic_one/task_3.py
@@ -0,0 +1,3 @@
+import datetime
+now=datetime.datetime.now()
+print(now.strftime('%Y-%M-%D %H-%M-%S'))
---
+++ python-programming/basic_one/task_4.py
@@ -0,0 +1,5 @@
+value=input("Enter the radius")
+r=float(value)
+pi=3.1416
+area=r*pi**2
+print(area)
