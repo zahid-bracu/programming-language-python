124a34ae95f4b4a66369d1ec4887a94a87e8b042
zahid-bracu <zahid.mym@gmail.com>
2022-01-03 20:27:58 +0600

    ok

---
+++ basic/6_module_classes_objects/1_module_importing/check.py
@@ -0,0 +1,5 @@
+def checkNumber(num):
+    if(num%2==0):
+        print("Even")
+    else:
+        print("Odd")
---
+++ basic/6_module_classes_objects/2_class_object/main.py
@@ -0,0 +1,9 @@
+from Student import Student
+
+std= Student("zahid","CSE",1.60,False)
+print(std.name)
+print(std.gpa)
+print(std.major)
+
+result=std.check_result()
+print(result)
